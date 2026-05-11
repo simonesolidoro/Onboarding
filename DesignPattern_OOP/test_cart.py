0d4a4f186a225fd0a1e2be4560a0f530a195b6f9
simonesolidoro <simone.solidoro@adcube.ai>
2026-05-11 15:07:31 +0200

    add test cart.total_price()

---
+++ DesignPattern_OOP/test_cart.py
@@ -0,0 +1,15 @@
+import unittest
+from carrello import Cart, Product,CartItem
+
+class TestCart(unittest.TestCase):
+    def test_add_product(self):
+        cart = Cart()
+        for i in range(10):
+            cart.add_product(Product("quaderno",5))
+            cart.add_product(Product("matita",1))
+        cart.remove_product(Product("quaderno",5))
+        self.assertEqual(55, cart.total_price())  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
